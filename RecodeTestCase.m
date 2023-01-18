ecee17d113da44a323a8e5a11cfdee2262f83283
Seth Bashford <seth.bashford@boystown.org>
2023-01-18 12:58:35 -0600

    free test

--- RecodeTestCase.m
+++ RecodeTestCase.m
@@ -16,5 +16,22 @@ classdef RecodeTestCase < matlab.unittest.TestCase
                 15257000       	1	4130
                 15885000       	1	512]);
         end
+
+        function tbd2(self)
+            events = [
+                16548000       	1	4116
+                18301000       	1	23
+                18308000       	1	4119
+                19803000       	1	4121
+                22305000       	1	33
+                22310000       	1	4129
+                22865000       	1	256
+                ];
+            self.assertEqual(recode(events), [
+                18308000       	1	4119
+                19803000       	1	4121
+                22310000       	1	4129
+                22865000       	1	256]);
+        end
     end
 end
