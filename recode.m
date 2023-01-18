41769922df52043d078cb04bcdc4645422c784f5
Seth Bashford <seth.bashford@boystown.org>
2023-01-18 15:13:06 -0600

    add test

--- recode.m
+++ recode.m
@@ -5,15 +5,16 @@ maintenanceLogicalIndices = diff([0; ismember(events(:, end), [25, 4121])]) == 1
 retrievalOnsetLogicalIndices = diff([0; ismember(events(:, end), [33, 34, 4129, 4130])]) == 1;
 encodingOnsetIndices = find(encodingOnsetLogicalIndices);
 retrievalOnsetIndices = find(retrievalOnsetLogicalIndices);
-for retrievalIndex = find(retrievalOnsetLogicalIndices).'
+for i = 1:numel(retrievalOnsetIndices)
+    retrievalIndex = retrievalOnsetIndices(i);
     responseIndex = retrievalIndex + 2;
     if responseIndex <= size(events, 1) && bitget(events(responseIndex, end), 8+1) && ismember(events(retrievalIndex, end), [33, 4129]) ...
             || responseIndex <= size(events, 1) && bitget(events(responseIndex, end), 9+1) && ismember(events(retrievalIndex, end), [34, 4130])
-        events(encodingOnsetIndices + 1, end) = bitset(events(encodingOnsetLogicalIndices, end), 12 + 1);
-        events(retrievalOnsetIndices + 1, end) = bitset(events(retrievalOnsetLogicalIndices, end), 12 + 1);
+        events(encodingOnsetIndices(i) + 1, end) = bitset(events(encodingOnsetIndices(i), end), 12 + 1);
+        events(retrievalOnsetIndices(i) + 1, end) = bitset(events(retrievalOnsetIndices(i), end), 12 + 1);
     else
-        events(encodingOnsetIndices + 1, end) = bitset(events(encodingOnsetLogicalIndices, end), 12 + 1) + 4000;
-        events(retrievalOnsetIndices + 1, end) = bitset(events(retrievalOnsetLogicalIndices, end), 12 + 1) + 4000;
+        events(encodingOnsetIndices(i) + 1, end) = bitset(events(encodingOnsetIndices(i), end), 12 + 1) + 4000;
+        events(retrievalOnsetIndices(i) + 1, end) = bitset(events(retrievalOnsetIndices(i), end), 12 + 1) + 4000;
     end
 end
 events(encodingOnsetLogicalIndices | retrievalOnsetLogicalIndices | fixationLogicalIndices, :) = [];
