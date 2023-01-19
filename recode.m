9e1e39290570c6a3608e2e8fc8a10927634a2df9
Seth Bashford <seth.bashford@boystown.org>
2023-01-19 13:05:59 -0600

    writing assertions for assumptions

--- recode.m
+++ recode.m
@@ -4,6 +4,8 @@ encodingOnsetLogicalIndices = diff([0; ismember(events(:, end), [23, 24, 4119, 4
 retrievalOnsetLogicalIndices = diff([0; ismember(events(:, end), [33, 34, 4129, 4130])]) == 1;
 encodingOnsetIndices = find(encodingOnsetLogicalIndices);
 retrievalOnsetIndices = find(retrievalOnsetLogicalIndices);
+assert(numel(encodingOnsetIndices) == numel(retrievalOnsetIndices), ...
+    "The number of encoding onset triggers does not match the number of retrieval onset triggers.");
 contributedByButton = logical(bitget(events(:, end), 8+1)) | logical(bitget(events(:, end), 9+1));
 for i = 1:numel(retrievalOnsetIndices)
     if i == numel(retrievalOnsetIndices)
@@ -13,15 +15,20 @@ for i = 1:numel(retrievalOnsetIndices)
     end
     retrievalIndex = retrievalOnsetIndices(i);
     responseIndex = find(contributedByButton(retrievalIndex+1:lastCandidateResponseIndex), 1) + retrievalIndex;
-
+    encodingOnsetTrigger = events(encodingOnsetIndices(i), end);
+    triggerFollowingEncodingOnset = events(encodingOnsetIndices(i) + 1, end);
+    assert(ismember(triggerFollowingEncodingOnset, [bitset(encodingOnsetTrigger, 12 + 1), 4096]), "Unexpected trigger following encoding onset.");
+    retrievalOnsetTrigger = events(retrievalOnsetIndices(i), end);
+    triggerFollowingRetrievalOnset = events(retrievalOnsetIndices(i) + 1, end);
+    assert(ismember(triggerFollowingRetrievalOnset, [bitset(retrievalOnsetTrigger, 12 + 1), 4096]), "Unexpected trigger following retrieval onset.");
     if ~isempty(responseIndex) ...
             && (bitget(events(responseIndex, end), 8+1) && ismember(events(retrievalIndex, end), [33, 4129]) ...
             || bitget(events(responseIndex, end), 9+1) && ismember(events(retrievalIndex, end), [34, 4130]))
-        events(encodingOnsetIndices(i) + 1, end) = bitset(events(encodingOnsetIndices(i), end), 12 + 1);
-        events(retrievalOnsetIndices(i) + 1, end) = bitset(events(retrievalOnsetIndices(i), end), 12 + 1);
+        events(encodingOnsetIndices(i) + 1, end) = bitset(encodingOnsetTrigger, 12 + 1);
+        events(retrievalOnsetIndices(i) + 1, end) = bitset(retrievalOnsetTrigger, 12 + 1);
     else
-        events(encodingOnsetIndices(i) + 1, end) = bitset(events(encodingOnsetIndices(i), end), 12 + 1) + 4000;
-        events(retrievalOnsetIndices(i) + 1, end) = bitset(events(retrievalOnsetIndices(i), end), 12 + 1) + 4000;
+        events(encodingOnsetIndices(i) + 1, end) = bitset(encodingOnsetTrigger, 12 + 1) + 4000;
+        events(retrievalOnsetIndices(i) + 1, end) = bitset(retrievalOnsetTrigger, 12 + 1) + 4000;
     end
 end
 events(encodingOnsetLogicalIndices | retrievalOnsetLogicalIndices | fixationLogicalIndices, :) = [];
