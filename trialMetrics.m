7a6a6d65f28b02162310a7b20d713e5737a9a870
Seth Bashford <seth.bashford@boystown.org>
2023-01-20 11:04:40 -0600

    refactor

--- trialMetrics.m
+++ trialMetrics.m
@@ -1,13 +1,20 @@
 function metrics = trialMetrics(events)
-retrievalIndices = find(ismember(events(:, end), [4129, 4130, 8129, 8130]));
+retrievalIndices = find(ismember(events(:, end), [...
+    bitset(33, BitTrigger.visual), ...
+    bitset(34, BitTrigger.visual), ...
+    bitset(33, BitTrigger.visual) + 4000, ...
+    bitset(34, BitTrigger.visual) + 4000]));
 metrics(numel(retrievalIndices)).correct = false;
 for i = 1:numel(retrievalIndices)
     index = retrievalIndices(i);
-    metrics(i).correct = ismember(events(index, end), [4129, 4130]);
+    metrics(i).correct = ismember(events(index, end), [...
+        bitset(33, BitTrigger.visual), ...
+        bitset(34, BitTrigger.visual)]);
     metrics(i).condition = conditionFromRetrievalIndex(events, index);
     metrics(i).reactionTimeMilliseconds = nan;
     if metrics(i).correct
-        responseIndex =  find(logical(bitget(events(index+1:end, end), 9)) | logical(bitget(events(index+1:end, end), 10)), 1) + index;
+        responseIndex =  find(logical(bitget(events(index+1:end, end), BitTrigger.buttonOne)) ...
+            | logical(bitget(events(index+1:end, end), BitTrigger.buttonTwo)), 1) + index;
         metrics(i).reactionTimeMilliseconds = (events(responseIndex, 1) - events(index, 1)) / 1000;
     end
 end
@@ -15,9 +22,9 @@ end
 
 function condition = conditionFromRetrievalIndex(events, index)
 switch events(index, end)
-    case {4129, 8129}
+    case {bitset(33, BitTrigger.visual), bitset(33, BitTrigger.visual) + 4000}
         condition = Condition.InSet;
-    case {4130, 8130}
+    case {bitset(34, BitTrigger.visual), bitset(34, BitTrigger.visual) + 4000}
         condition = Condition.OutOfSet;
 end
 end
